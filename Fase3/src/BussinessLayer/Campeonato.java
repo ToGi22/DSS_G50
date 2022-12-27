62e7adfa8244773f8c84ec7735cc912f0c5cc83b
0xAbelheira <h.abelheira@gmail.com>
2022-12-27 22:53:21 +0000

    Classe Campeonato

--- Fase3/src/BussinessLayer/Campeonato.java
+++ Fase3/src/BussinessLayer/Campeonato.java
@@ -2,6 +2,7 @@ package BussinessLayer;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 /**
  * Write a description of class Campeonato here.
@@ -22,4 +23,37 @@ public class Campeonato{
         this.nrMaxParticipantes = 0;
         this.circuitosIntegrantes = new ArrayList<Circuito>();
     }
+
+    public Campeonato(String nomeCamp, int nrCorridas, int nrMaxParticipantes, ArrayList<Circuito> circuitosIntegrantes){
+        this.nomeCamp = nomeCamp;
+        this.nrCorridas = nrCorridas;
+        this.nrMaxParticipantes = nrMaxParticipantes;
+        this.circuitosIntegrantes = circuitosIntegrantes;
+    }
+
+    public Campeonato(Campeonato campeonato){
+        this.nomeCamp = campeonato.getNomeCamp();
+        this.nrCorridas = campeonato.getNrCorridas();
+        this.nrMaxParticipantes = campeonato.getNrMaxParticipantes();
+        this.circuitosIntegrantes = campeonato.getCircuitosIntegrantes();
+    }
+
+    public String getNomeCamp(){
+        return this.nomeCamp;
+    }
+
+    public int getNrCorridas(){
+        return this.nrCorridas;
+    }
+
+    public int getNrMaxParticipantes(){
+        return this.nrMaxParticipantes;
+    }
+
+    public ArrayList<Circuito> getCircuitosIntegrantes(){
+        //return (ArrayList<Circuito>) new ArrayList<>(this.circuitosIntegrantes);
+        return (ArrayList<Circuito>) this.circuitosIntegrantes.stream().collect(Collectors.toList());
+    }
+
+
 }
