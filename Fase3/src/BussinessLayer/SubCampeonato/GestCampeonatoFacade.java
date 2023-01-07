423b7951bb196c8b52677226bcf41be80f748bc5
0xAbelheira <h.abelheira@gmail.com>
2023-01-07 17:53:08 +0000

    changes

--- Fase3/src/BussinessLayer/SubCampeonato/GestCampeonatoFacade.java
+++ Fase3/src/BussinessLayer/SubCampeonato/GestCampeonatoFacade.java
@@ -54,11 +54,11 @@ public class GestCampeonatoFacade implements ICampeonato{
 
     // Função para validar o nome do campeonato
     // O nome deve começar com uma letra
-    // O nome deve ter entre 5 a 29 caracteres
+    // O nome deve ter entre 3 a 29 caracteres
     public static boolean validaNomeCamp(String nomeCamp)
     {
 
-        String regex = "^[A-Za-z]\\w{5,29}$";
+        String regex = "^[A-Za-z]\\w{3,29}$";
 
         Pattern p = Pattern.compile(regex);
 
@@ -72,11 +72,11 @@ public class GestCampeonatoFacade implements ICampeonato{
 
     // Função para validar o nome do circuito
     // O nome deve começar com uma letra
-    // O nome deve ter entre 5 a 29 caracteres
+    // O nome deve ter entre 3 a 29 caracteres
     public static boolean validaNomeCir(String nomeCir)
     {
 
-        String regex = "^[A-Za-z]\\w{5,29}$";
+        String regex = "^[A-Za-z]\\w{3,29}$";
 
         Pattern p = Pattern.compile(regex);
 
