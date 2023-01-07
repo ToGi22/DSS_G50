ac111e59d585ea99a16070ae21bfce68bbe73142
0xAbelheira <h.abelheira@gmail.com>
2023-01-07 20:49:55 +0000

    Added interfaces

--- Fase3/src/BussinessLayer/SubCampeonato/ICampeonato.java
+++ Fase3/src/BussinessLayer/SubCampeonato/ICampeonato.java
@@ -1,4 +1,10 @@
 package BussinessLayer.SubCampeonato;
 
+import java.util.ArrayList;
+
 public interface ICampeonato {
+    public void registaCircuito(String nomeCir, double distancia, int numeroVoltas, ArrayList<Segmentos> listaSegmentos);
+    public void registaCampeonato(String nomeCamp, int nrMaxParticipantes, ArrayList<String> listaCircuitosIntegrantes);
+    public boolean verificaNomeCamp(String nomeCamp);
+    public boolean verificaNomeCir(String nomeCir);
 }
